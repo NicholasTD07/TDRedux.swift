dc4ca77c1d2e972dca2955ee5cd02ce0d4683f37
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-09-24 16:20:08 +1000

    Add .gitignore

---
+++ TDRedux.swift/ReducerHelpers.swift
@@ -0,0 +1,9 @@
+//
+//  ReducerHelpers.swift
+//  TDRedux.swift
+//
+//  Created by Nicholas Tian on 24/09/2016.
+//  Copyright © 2016 nicktd. All rights reserved.
+//
+
+import Foundation
---
+++ TDRedux.swift/Store.swift
@@ -0,0 +1,9 @@
+//
+//  Store.swift
+//  TDRedux.swift
+//
+//  Created by Nicholas Tian on 24/09/2016.
+//  Copyright © 2016 nicktd. All rights reserved.
+//
+
+import Foundation
