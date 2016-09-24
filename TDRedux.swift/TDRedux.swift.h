7a6dcc82f39cf8eec66483c1acb22524fe7134a2
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-09-24 15:43:59 +1000

    Initial Commit

---
+++ TDRedux.swift/TDRedux.swift.h
@@ -0,0 +1,19 @@
+//
+//  TDRedux.swift.h
+//  TDRedux.swift
+//
+//  Created by Nicholas Tian on 24/09/2016.
+//  Copyright © 2016 nicktd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TDRedux.swift.
+FOUNDATION_EXPORT double TDRedux_swiftVersionNumber;
+
+//! Project version string for TDRedux.swift.
+FOUNDATION_EXPORT const unsigned char TDRedux_swiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDRedux_swift/PublicHeader.h>
+
+
