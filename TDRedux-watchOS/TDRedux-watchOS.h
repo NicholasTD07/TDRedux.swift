9253c8ede828626c9586deceb5495ec7bda562ed
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-09-25 19:24:40 +1000

    Add watchOS target and some other files

---
+++ TDRedux-watchOS/TDRedux-watchOS.h
@@ -0,0 +1,19 @@
+//
+//  TDRedux-watchOS.h
+//  TDRedux-watchOS
+//
+//  Created by Nicholas Tian on 25/09/2016.
+//  Copyright © 2016 nicktd. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for TDRedux-watchOS.
+FOUNDATION_EXPORT double TDRedux_watchOSVersionNumber;
+
+//! Project version string for TDRedux-watchOS.
+FOUNDATION_EXPORT const unsigned char TDRedux_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TDRedux_watchOS/PublicHeader.h>
+
+
