3799dc500adb5d2dcdcdfd0d6654aca0e2595c29
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-10-18 19:26:01 +1100

    Add docs for new `subscribe` methods

--- Sources/Store+Subscribe.swift
+++ Sources/Store+Subscribe.swift
@@ -9,12 +9,23 @@
 extension Store {
     public typealias StateSubscriber = (_ state: State) -> ()
 
+    /// Subcribes a *StateSubscriber* (takes a *State* rather than a *Store*)
+    /// to the changes of a *Store*'s *State*
+    ///
+    /// - parameter subscriber: A StateSubscriber function
     public func subscribe(with subscriber: @escaping StateSubscriber) {
         self.subscribe { store in
             subscriber(store.state)
         }
     }
 
+
+    /// Subcribes a *SpecificStateSubscriber* (takes a *SpecificState*)
+    /// to the changes of a *Store*'s *State*
+    /// with the help of a `Converter`
+    ///
+    /// - parameter converter:  A function converts State to SpecificState
+    /// - parameter subscriber: A SpecificStateSubscriber function
     public func subscribe<SpecificState>(
         withConverter converter: @escaping (State) -> SpecificState,
         subscriber: @escaping (SpecificState) -> ()
