30d920cbbc24eaa924862aab66049095dda59f13
NicholasTD07 <Nicholas.TD07@gmail.com>
2016-10-20 17:58:24 +1100

    Add empty DispatchExtensionSpecs

---
+++ Tests/DispatchExtensionSpecs.swift
@@ -0,0 +1,15 @@
+//
+//  DispatchExtensionSpecs.swift
+//  TDRedux
+//
+//  Created by Nicholas Tian on 20/10/2016.
+//  Copyright © 2016 nicktd. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+import TDRedux
+
+class DispatchExtensionSpecs: QuickSpec {
+}
