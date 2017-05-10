81acc74628468b18acaa468aa9d9f0d53ef331f4
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-05-10 14:12:06 +1000

    Don't think the `dylib` part is needed

--- Package.swift
+++ Package.swift
@@ -3,11 +3,3 @@ import PackageDescription
 let package = Package(
     name: "TDRedux"
 )
-
-let dylib = Product(
-    name: "TDRedux",
-    type: .Library(.Dynamic),
-    modules: "TDRedux"
-)
-
-products.append(dylib)
