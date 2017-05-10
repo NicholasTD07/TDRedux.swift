73a9886640592d1e8da020b8ada93dd440b96e09
NicholasTD07 <NicholasTD07@users.noreply.github.com>
2017-05-10 15:29:27 +1000

    Add spec for when dispatching actions, the state changes

--- Tests/TDReduxTests/StoreTests.swift
+++ Tests/TDReduxTests/StoreTests.swift
@@ -9,7 +9,14 @@ func storeSpec() {
 
         $0.context("when created") {
             $0.before { store = Store<ToDoState>.init(with: todoReducer) }
+
             $0.it("has the initial state") { expect(store.state) == ToDoState.initial }
+
+            $0.context("when dispatched action to add a todo") {
+                $0.before { store.dispatch(ToDoState.Action.addToDo("get milk")) }
+
+                $0.it("has the todo") { expect(store.state.todos).to.contain("get milk") }
+            }
         }
     }
 }
